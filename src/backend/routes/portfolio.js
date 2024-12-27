5b2273512dd22f5a82bdc145602bb5b76f1a8eb0
Mariam Elsherbiny <138718292+mariamelsherbinyy@users.noreply.github.com>
2024-12-27 04:07:44 +0200

    Delete src/backend/routes directory

--- src/backend/routes/portfolio.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadPortfolio, setVisibility } = require("../controllers/portfolio");
-
-router.post("/upload", uploadPortfolio);
-router.put("/visibility", setVisibility);
-
-module.exports = router;
