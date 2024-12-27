cf6a0fe1d84b7f4d1f561b261fdac49c48c7c984
Mariam Elsherbiny <138718292+mariamelsherbinyy@users.noreply.github.com>
2024-12-27 04:26:59 +0200

    Create Homepage.js

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>ArtLoft</h1>
+            <div>Hero Section</div>
+            <div>Event Calendar</div>
+            <div>Artist Spotlight</div>
+            <div>Search and Filter Options</div>
+        </div>
+    );
+};
+
+export default Homepage;
